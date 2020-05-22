6b0c7294aeb87257ee4274d92fca666342fdb50a
FiniteReality <digpoealternate@gmail.com>
2020-05-22 21:04:12 +0100

    Add actual an unit test

---
+++ tests/Core/MetricsServiceCollectionExtensionsTests.cs
@@ -0,0 +1,86 @@
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Finite.Metrics.Core.UnitTests
+{
+    /// <summary>
+    /// Unit tests for <see cref="MetricsServiceCollectionExtensions"/>
+    /// </summary>
+    public class MetricsServiceCollectionExtensionsTests
+    {
+        /// <summary>
+        /// Ensures the correct metrics services have been added to the service
+        /// collection when using the AddMetrics extension method.
+        /// </summary>
+        [Test]
+        public void MetricsServicesAreAddedToCollection()
+        {
+            var collection = new ServiceCollection();
+
+            _ = collection.AddMetrics();
+
+            var hasIMetricFactory = collection.Any(
+                x => x.ServiceType == typeof(IMetricFactory));
+            var hasIMetric = collection.Any(
+                x => x.ServiceType == typeof(IMetric));
+
+            Assert.True(hasIMetricFactory && hasIMetric);
+        }
+
+        /// <summary>
+        /// Ensures that the <see cref="MetricsServiceCollectionExtensions.AddMetrics(IServiceCollection)"/>
+        /// method can be called multiple times without throwing exceptions.
+        /// </summary>
+        [Test]
+        public void MetricsServicesCanBeAddedMultipleTimes()
+        {
+            var collection = new ServiceCollection();
+
+            _ = collection.AddMetrics();
+
+            Assert.DoesNotThrow(() => collection.AddMetrics());
+        }
+
+        /// <summary>
+        /// Ensures that the <see cref="MetricsServiceCollectionExtensions.AddMetrics(IServiceCollection, System.Action{IMetricsBuilder})"/>
+        /// method is called.
+        /// </summary>
+        [Test]
+        public void AddMetricsWithDelegateCallsDelegate()
+        {
+            var collection = new ServiceCollection();
+
+            _ = collection.AddMetrics(_ => Assert.Pass());
+
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Ensures that the <see cref="MetricsServiceCollectionExtensions.AddMetrics(IServiceCollection, System.Action{IMetricsBuilder})"/>
+        /// method is called.
+        /// </summary>
+        [Test]
+        public void AddMetricsWithDelegateCalledWithIMetricsBuilder()
+        {
+            var collection = new ServiceCollection();
+
+            _ = collection.AddMetrics(builder => Assert.NotNull(builder));
+        }
+
+        /// <summary>
+        /// Ensures that the <see cref="MetricsServiceCollectionExtensions.AddMetrics(IServiceCollection, System.Action{IMetricsBuilder})"/>
+        /// method is passed an <see cref="IMetricsBuilder"/>, whose
+        /// <see cref="IMetricsBuilder.Services"/> property equals the
+        /// container we called <c>AddMetrics</c> on.
+        /// </summary>
+        [Test]
+        public void AddMetricsWithDelegateHasSameContainer()
+        {
+            var collection = new ServiceCollection();
+
+            _ = collection.AddMetrics(
+                builder => Assert.AreEqual(collection, builder.Services));
+        }
+    }
+}
